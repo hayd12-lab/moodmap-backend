12076f59ad247d471fea762fc9b26b9c483bd66f
hayd12-lab <hayoungi825@gmail.com>
2026-07-23 15:53:59 +0900

    feet: 초기 코드 커밋

---
+++ src/main/java/com/moodmap/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.moodmap.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
